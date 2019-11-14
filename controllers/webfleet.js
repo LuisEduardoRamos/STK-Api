1f8e5b60e4c453ad88081817937aac5ce17f21de
victormorfin97 <victormorfin97@gmail.com>
2019-11-13 18:23:24 -0700

    sd

--- controllers/webfleet.js
+++ controllers/webfleet.js
@@ -14,7 +14,7 @@ let inrouteCentral_url = process.env.INROUTE_CENTRAL_URL;
 //------------------------------------------------------------------------- Rutina todos los días a las 4:00 A.M.----------------------------------------------------------------------------------------------------//
 let rule = new schedule.RecurrenceRule();
 rule.hour = 1;
-rule.minute = 22;
+rule.minute = 25;
 let automaticDailySyncBD = schedule.scheduleJob(rule, async function () {
     let dormir = false;
     try{
@@ -110,7 +110,6 @@ async function autoSyncBD(cliente) {
     let arrayPopQueue = []
     let msgObject = {}
     let statusResp = 200;
-    let cliente = req.user;
     let messageQueue = [];
     let clear = null;
     try {
